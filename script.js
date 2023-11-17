6f51bbb594a486c3940e7350c639d48e21c8f90d
Freenet <150752410+freetowatch@users.noreply.github.com>
2023-11-16 17:46:49 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function toggleDropdown() {
+    var dropdown = document.getElementById("dropdown-content");
+    dropdown.style.display = (dropdown.style.display === "none" || dropdown.style.display === "") ? "block" : "none";
+}
